02f200628a462864f2fd8eb671f0f5dfda62c48a
noiraimer <noiraimer@users.noreply.github.com>
2020-11-13 15:26:18 +0000

    Automated deployment: Fri Nov 13 15:26:18 UTC 2020 a6f5cbd89d2af400c75532ab623adddad7e39cf1

--- js/time-0003.js
+++ js/time-0003.js
@@ -13,13 +13,20 @@ function time(){//时间函数
 	//var week="Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday".charAt(date.getDay());//获取此刻周几
 	
 	var weekday=new Array(7)
-	weekday[0]="Sunday"
+	weekday[0]="周日"
+	weekday[1]="周一"
+	weekday[2]="周二"
+	weekday[3]="周三"
+	weekday[4]="周四"
+	weekday[5]="周五"
+	weekday[6]="周六"
+/* 	weekday[0]="Sunday"
 	weekday[1]="Monday"
 	weekday[2]="Tuesday"
 	weekday[3]="Wednesday"
 	weekday[4]="Thursday"
 	weekday[5]="Friday"
-	weekday[6]="Saturday"
+	weekday[6]="Saturday" */
 	//根据id="time"获取span标签并调用innerHTML方法为标签内添加内容
 	document.getElementById("ga-time-day").innerHTML = "今天是"+years+"年"+month+"月"+day+"日";
 	document.getElementById("ga-time-week").innerHTML = "&nbsp"+weekday[date.getDay()];
